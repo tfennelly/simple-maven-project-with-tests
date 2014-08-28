58d8387fdf2a322eb973ed688cf6d035f5541a63
tfennelly <tom.fennelly@gmail.com>
2014-08-28 10:44:26 +0100

    https://issues.jenkins-ci.org/browse/JENKINS-24407

--- src/test/java/test/OtherTest.java
+++ src/test/java/test/OtherTest.java
@@ -1,11 +1,19 @@
 package test;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-public class OtherTest extends Base {
+public class OtherTest {
 
-    @Test public void mytest() {
-        run();
+    @Test
+    public void mytest() {
+        // Fail every one in five (or so)
+//        if (System.currentTimeMillis() % 5 == 0) {
+//            Assert.fail("Failed");
+//        }
+        if (System.currentTimeMillis() % 5 == 0) {
+            Manzer.main(new String[] {"hi"});
+        }
     }
 
 }
