43253e1bb58480c3c5c9b5445c8dae7452240f34
Tom Fennelly <tom.fennelly@gmail.com>
2015-07-15 13:45:09 +0100

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -2,7 +2,7 @@ node {
     def mvnHome = tool 'M3'
 
     stage 'Checkout'
-    git git url: 'https://github.com/tfennelly/simple-maven-project-with-tests.git'
+    git url: 'https://github.com/tfennelly/simple-maven-project-with-tests.git'
     
     stage 'Build'
     sh "${mvnHome}/bin/mvn clean install"
