29e4c1b7a847c80461186f3fe480a23dca9302b6
tfennelly <tom.fennelly@gmail.com>
2017-05-02 16:45:16 +0100

    CBN-157 back to A

--- src/main/java/test/Manzer.java
+++ src/main/java/test/Manzer.java
@@ -9,25 +9,7 @@ public class Manzer {
         System.out.println("Live from Palo Alto!!");
 
         System.out.println("A");
-        System.out.println("B");
-        System.out.println("C");
-        System.out.println("D");
-        System.out.println("E");
-        System.out.println("F");
-        System.out.println("G");
-        System.out.println("H");
-        System.out.println("I");
-        System.out.println("J");
-        System.out.println("K");
-        System.out.println("L");
-        System.out.println("M");
-        System.out.println("N");
-        System.out.println("O");
-        System.out.println("P");
-        System.out.println("Q");
-        System.out.println("R");
-        System.out.println("S");
-        breakme
+        // breakme
     }
 
 }
