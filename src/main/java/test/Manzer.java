4cfd0aec77b5992d878acce2d4026c8ee4ffa5e8
tfennelly <tom.fennelly@gmail.com>
2017-05-15 13:48:35 +0100

    CBN-192 do G

--- src/main/java/test/Manzer.java
+++ src/main/java/test/Manzer.java
@@ -15,6 +15,7 @@ public class Manzer {
         System.out.println("D");
         System.out.println("E");
         System.out.println("F");
+        System.out.println("G");
         //breakme
     }
 
