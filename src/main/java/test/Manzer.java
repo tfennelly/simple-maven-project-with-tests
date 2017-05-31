bb4a9cc0c0d7985d5bbd322146a846374e939abc
tfennelly <tom.fennelly@gmail.com>
2017-05-31 18:19:55 -0400

    CBN-202 back to B

--- src/main/java/test/Manzer.java
+++ src/main/java/test/Manzer.java
@@ -11,20 +11,6 @@ public class Manzer {
         System.out.println("A");
         System.out.println("B");
         System.out.println("B");
-        System.out.println("C");
-        System.out.println("D");
-        System.out.println("E");
-        System.out.println("F");
-        System.out.println("G");
-        System.out.println("H");
-        System.out.println("I");
-        System.out.println("J");
-        System.out.println("K");
-        System.out.println("L");
-        System.out.println("M");
-        System.out.println("N");
-        System.out.println("O");
-        System.out.println("P");
         //breakme
     }
 
