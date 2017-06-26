dfdbfac9285dfd83acc8d2e2d583a6cf02a8a2b7
tfennelly <tom.fennelly@gmail.com>
2017-06-26 10:15:29 +0100

    CBN-208 fix H

--- src/main/java/test/Manzer.java
+++ src/main/java/test/Manzer.java
@@ -15,6 +15,7 @@ public class Manzer {
         System.out.println("E");
         System.out.println("F");
         System.out.println("G");
+        System.out.println("H");
         //breakme
     }
 
